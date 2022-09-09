fa52097167028ad77ecfd75c06f526c658449f8c
bedirhandogan <bedirhandgn14@gmail.com>
2022-09-09 21:54:52 +0300

    feat: empty error

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -4,6 +4,9 @@ import Item from "components/List/Item";
 function List({deleteItem, itemCompleted, editValue, tasks}) {
     return (
         <div className={'list-container'}>
+            { tasks.length === 0 && <div className={'empty'}>
+                No items found
+            </div>}
             { tasks && tasks.map(item => <Item item={item} deleteItem={deleteItem}
                                                itemCompleted={itemCompleted}
                                                editValue={editValue}
