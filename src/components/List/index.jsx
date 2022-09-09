ea198e18f06678db418128f1fa6b1119368be41b
bedirhandogan <bedirhandgn14@gmail.com>
2022-09-09 22:09:30 +0300

    feat: AutoAnimate

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,9 +1,11 @@
 import './styles.css';
 import Item from "components/List/Item";
+import {useAutoAnimate} from "@formkit/auto-animate/react";
 
 function List({deleteItem, itemCompleted, editValue, tasks}) {
+    const [animationParent] = useAutoAnimate();
     return (
-        <div className={'list-container'}>
+        <div className={'list-container'} ref={animationParent}>
             { tasks.length === 0 && <div className={'empty'}>
                 No items found
             </div>}
